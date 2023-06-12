e9e004e7aba7c109b8dcdf0f5cc55271aaa7f882
Nicholas Keller-Sedan <njkeller@uwaterloo.ca>
2023-06-12 15:27:27 -0400

    added 3 stories

---
+++ madlibs.py
@@ -0,0 +1,56 @@
+"Madlibs"
+
+dragon = [
+  "Dragon Adventure",
+  """The [COLOUR] Dragon is the [SUPERLATIVE (ENDING IS "EST")] Dragon of all. It has [ADJECTIVE] [BODY PARTS (Plural)], and a(n) [BODY PART] shaped like a(n) [NOUN]. It loves to eat [ANIMAL (Plural)], although it will feast on nearly anything. It is [ADJECTIVE2] and [ADJECTIVE3]. You must be [ADJECTIVE4] around it, or you may end up as it`s meal!""",
+  [
+    ["Select a colour: ", "[COLOUR]"],
+    ["Select a superlative adjective ending in est: ", '[SUPERLATIVE (ENDING IS "EST")]'],
+    ["Select an adjective: ", "[ADJECTIVE]"],
+    ["Select a plural body part: ", "[BODY PARTS (Plural)]"],
+    ["Select a body part: ", "[BODY PART]"],
+    ["Select a noun: ", "[NOUN]"],
+    ["Select a plural animal: ", "[ANIMAL (Plural)]"],
+    ["Select an adjective: ", "[ADJECTIVE2]"],
+    ["Select another adjective: ", "[ADJECTIVE3]"],
+    ["Select another adjective: ", "[ADJECTIVE4]"],
+  ]
+]
+
+pizza = [
+  "Pizza Party",
+  """I just got back from a pizza party with [THINGS (Plural)]. Can you believe we got to eat [ADJECTIVE] pizza in the [MONUMENT]?! Everyone got to choose their own toppings. I made '[FOOD] and [FOOD2]' pizza, which is my favorite! They even stuffed the crust with [FOOD3]. How [ADJECTIVE2]! If that wasn't good enough already, [SINGER] was there singing [SONG]. I was so inspired by the music, I had to get up out of my seat and [ADJECTIVE3].""",
+  [
+    ["Select a plural thing: ", "[THINGS (Plural]"],
+    ["Select an adjective: ", "[ADJECTIVE]"],
+    ["Select a monument: ", "[MONUMENT]"],
+    ["Select a food: ", "[FOOD]"],
+    ["Select another food: ", "[FOOD2]"],
+    ["Select another food: ", "[FOOD3]"],
+    ["Select an adjective: ", "[ADJECTIVE2]"],
+    ["Select a singer: ", "[SINGER]"],
+    ["Select a song: ", "[SONG]"],
+    ["Select an adjective: ", "[ADJECTIVE3]"],
+  ]
+]
+
+movies = [
+  "Movies",
+  """I went to the movies yesterday with [NAME] and [NAME2]. We saw [MOVIE]. It was [ADJECTIVE]. At one part, I even got scared and ran for the [NOUN]. During the movie, we ate [CANDY] and [FOOD]. I got mad because the person sitting behind me kept [VERB] during the movie and wouldn't stop [VERB2]. He was asked to leave after he [VERB3] across the theatre. It was pretty [ADJECTIVE2]. Overall, I liked the movie because it was [ADJECTIVE3] and the main character was super [ADJECTIVE4]. Hopefully next time the people sitting behind me will be more [ADJECTIVE5].""",
+  [
+    ["Select a name: ", "[NAME]"],
+    ["Select another name: ", "[NAME2]"],
+    ["Select a movie: ", "[MOVIE]"],
+    ["Select an adjective: ", "[ADJECTIVE]"],
+    ["Select a noun: ", "[NOUN]"],
+    ["Select a candy: ", "[CANDY]"],
+    ["Select a food: ", "[FOOD]"],
+    ["Select a verb: ", "[VERB]"],
+    ["Select another verb: ", "[VERB2]"],
+    ["Select another verb: ", "[VERB3]"],
+    ["Select an adjective: ", "[ADJECTIVE2]"],
+    ["Select another adjective: ", "[ADJECTIVE3]"],
+    ["Select another adjective: ", "[ADJECTIVE4]"],
+    ["Select another adjective: ", "[ADJECTIVE5]"],
+  ]
+]
