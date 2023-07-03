f2b6c045020b9ae3e1406b3eb6425329a6f979ce
Nicholas Keller-Sedan <njkeller@uwaterloo.ca>
2023-07-02 23:13:30 -0400

    added winner outcome for each choice

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -26,15 +26,15 @@ rules = [
 
 opponent_type = [
     "Computer",
-    "Human"
+    "Player 2"
     ]
 
 choices = [
-    ["Rock"],
-    ["Paper"],
-    ["Scissors"],
-    ["Lizard"],
-    ["Spock"],
+    "Rock",
+    "Paper",
+    "Scissors",
+    "Lizard",
+    "Spock",
     ]
 
 print("\033[1m Welcome to Rock Paper Scissors!\033[0m")
@@ -90,7 +90,7 @@ while selectedVS != "1" or selectedVS != "2":
             break
         elif selectedVS == "2":
             opponent = opponent_type[1]
-            print("\nThe " + opponent + " is your opponent!")
+            print("\n" + opponent + " is your opponent!")
             break
     else:
         print("Invalid Option!, you needed to type a 1, or 2")
@@ -106,29 +106,74 @@ else:
 
 # Player Selection of options
 
-player_choice = input("Player 1 select your choice: ") 
-print(game_options[int(player_choice) - 1])
-
+player_selection = input("Player 1 select your choice: ") 
+player_choice = game_options[int(player_selection) - 1]
+print(player_choice)
 
 # If statement on if it is 2 player or vs computer
 if selectedVS == "1":
     # Computers Selection of options
     max_choices = int(len(game_options))
     cpu_choice = random.randint(0,max_choices)
-    print("The computer chose: " + str(game_options[int(cpu_choice) - 1]))
+    opponent_choice = game_options[int(cpu_choice) - 1]
+    print("The computer chose: " + opponent_choice)
 elif selectedVS == "2":
     # Player
-    opponent_choice = input("Player 2 select your choice: ")
-    print(game_options[int(opponent_choice) - 1])
+    opponent_selection = input("Player 2 select your choice: ")
+    opponent_choice = game_options[int(opponent_selection) - 1]
+    print(opponent_choice)
 
 
 # Which options beat which
-
-
 # Displaying Outcome of options (Win/Loss/Draw)
 
+if player_choice == opponent_choice:
+    print(f"Both players selected {player_choice}. It's a tie!")
+elif player_choice == "Rock":
+    if opponent_choice == "Scissors":
+        print("Rock crushes scissors! Player 1 wins!")
+    elif opponent_choice == "Lizard":
+        print("Rock crushes lizard! Player 1 wins!")
+    elif opponent_choice == "Spock":
+        print(f"Spock vaporizes rock! {opponent} wins!")
+    else:
+        print(f"Paper covers rock! {opponent} wins!")
+elif player_choice == "Paper":
+    if opponent_choice == "Rock":
+        print("Paper covers rock! Player 1 wins!")
+    elif opponent_choice == "Spock":
+        print("Spock disproves paper! Player 1 wins!")
+    elif opponent_choice == "Lizard":
+        print(f"Lizard eats paper! {opponent} wins!")
+    else:
+        print(f"Scissors cuts paper! {opponent} wins!")
+elif player_choice == "Scissors":
+    if opponent_choice == "Paper":
+        print("Scissors cuts paper! Player 1 wins!")
+    elif opponent_choice == "Lizard":
+        print("Scissors decapitates lizard! Player 1 wins!")
+    elif opponent_choice == "Spock":
+        print(f"Spock smashes scissors! {opponent} wins!")
+    else:
+        print(f"Rock smashes scissors! {opponent} wins!") 
+elif player_choice == "Lizard":
+    if opponent_choice == "Paper":
+        print("Lizard eats paper! Player 1 wins!")
+    elif opponent_choice == "Spock":
+        print("Lizard poisons Spock! Player 1 wins!")
+    elif opponent_choice == "Scissors":
+        print(f"Scissors decapitates lizard! {opponent} wins!.")
+    else:
+        print(f"Rock crushes lizard! {opponent} wins!")
+elif player_choice == "Spock":
+    if opponent_choice == "Rock":
+        print("Spock vaporizes rock! Player 1 wins!")
+    elif opponent_choice == "Scissors":
+        print("Spock smashes scissors! Player 1 wins!")
+    elif opponent_choice == "Lizard":
+        print(f"Lizard poisons Spock! {opponent} wins!.")
+    else:
+        print(f"Spock disproves paper! {opponent} wins!")   
 
-# Features to add
-# Ask how many rounds are being played
 # Display Score
 # Play again feature
